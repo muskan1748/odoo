d8093999ddfdc2575f8312ee8267c6ecb65c276f
muskan1748 <muskan.sodhi@technians.com>
2024-02-26 17:40:56 +0530

    first commit

---
+++ hospital_management/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'hospital management',
+    'author': 'odoo16',
+    'website': 'www.google.com',
+    'summary': 'hospital management',
+    'depends':['mail'],
+    'data': [
+            'security/ir.model.access.csv',
+            'views/menu.xml',
+            'views/patient.xml'
+            ],
+    'images': ['static/description/icon.gif'],
+    'installable': True,
+    'auto_install': False,
+}
