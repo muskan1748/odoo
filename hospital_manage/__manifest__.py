cc763bea9db8d6f605c27235720469a14ef4f4d9
muskan1748 <muskan.sodhi@technians.com>
2024-02-28 11:21:39 +0530

    test1

--- hospital_manage/__manifest__.py
+++ hospital_manage/__manifest__.py
@@ -1,14 +1,16 @@
 {
     'name': 'hospital management',
     'author': 'odoo16',
+    'version':'16.0',
     'website': 'www.google.com',
     'summary': 'hospital management',
-    'depends':['mail'],
+    'depends':['base, mail'],
     'data': [
             'security/ir.model.access.csv',
             'views/menu.xml',
             'views/patient.xml'
             ],
+    'images': ['static/description/icon.png'],
     'images': ['static/description/banner.gif'],
     'installable': True,
     'auto_install': False,
